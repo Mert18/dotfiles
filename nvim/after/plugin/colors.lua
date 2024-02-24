4ab6e70bd086e596ac496b7bd5ae2564b7a98487
mert18 <mertuygur02@gmail.com>
2024-02-25 00:33:13 +0300

    Shiny wallpaper for kitty, nvim settings reloaded

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,11 @@
 function Colorize(color)
-	color = color or "rose-pine"
+	color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg= "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg= "none" })
+	vim.cmd[[hi SignColumn guibg=NONE]]
+	vim.cmd[[highlight clear StatusLine]]
 end
 
 Colorize()
